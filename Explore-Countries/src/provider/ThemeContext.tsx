3f3b58b3d84aa0022c73d59d3dd0306281d79b39
Praise <praisetoyo@gmail.com>
2025-03-01 00:09:35 +0100

    feat: set up mode switch

---
+++ Explore-Countries/src/provider/ThemeContext.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { createContext, ReactNode, useCallback, useContext, useEffect, useMemo, useState } from 'react';
+
+interface ThemeContextProps {
+  isDarkMode: boolean;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
+
+export const ThemeProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme === 'dark') {
+      setIsDarkMode(storedTheme === 'dark');
+    } else {
+      setIsDarkMode(window.matchMedia('(prefers-color-scheme: dark)').matches);
+    }
+
+    const darkModeListener = (e: MediaQueryListEvent) => {
+      setIsDarkMode(e.matches);
+    };
+    const darkModeQuery = window.matchMedia('prefers-color-scheme: dark');
+    darkModeQuery.addEventListener('change', darkModeListener);
+
+    return () => {
+      darkModeQuery.removeEventListener('change', darkModeListener);
+    };
+  }, []);
+
+  const toggleTheme = useCallback(() => {
+    const newTheme = isDarkMode ? 'light' : 'dark';
+    setIsDarkMode(!isDarkMode);
+    localStorage.setItem('theme', newTheme);
+    document.documentElement.dataset.theme = newTheme;
+  }, [isDarkMode]);
+
+  useEffect(() => {
+    document.documentElement.dataset.theme = isDarkMode ? 'dark' : 'light';
+  }, [isDarkMode]);
+
+  return useMemo(
+    () => (
+      <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>{children}</ThemeContext.Provider>
+    ),
+    [children, isDarkMode, toggleTheme]
+  );
+};
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+  return context;
+};
