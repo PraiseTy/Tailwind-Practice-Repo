8745399f54d89a57242a00d53ce2bdae80a6e436
Praise <praisetoyo@gmail.com>
2025-03-01 16:38:19 +0100

    feat: finish navbar styling

--- Explore-Countries/src/components/Navbar/Navbar.tsx
+++ Explore-Countries/src/components/Navbar/Navbar.tsx
@@ -6,20 +6,23 @@ import { useTheme } from '@/provider/ThemeContext';
 const Navbar = () => {
   const { isDarkMode, toggleTheme } = useTheme();
   return (
-    <div className='flex flex-row'>
-      <div className='text-2xl'>Where in the World?</div>
-      <button onClick={toggleTheme}>
-        {isDarkMode ?
-          <div>
-            <Image src="/sun.svg" alt="light" width={20} height={20} />
+    <div className="bg-[var(--color-navColor)]">
+      <div className="flex flex-row justify-between container w-full mx-auto py-6 max-sm:mx-0 max-sm:py-12 max-sm:px-6">
+        <div className="text-2xl max-sm:text-lg font-semibold">Where in the World?</div>
+        <button onClick={toggleTheme}>
+          <div className="flex items-center justify-center space-x-2 max-sm:text-sm">
+            {isDarkMode ?
+              (<Image src="/lightMoon.svg" alt="light" width={20} height={20} />)
+              :
+              (<Image src="/darkMoon.svg" alt="dark" width={20} height={20} />)
+            }
             <p>Dark Mode</p>
           </div>
-          : <div>
-            <Image src="/moon.svg" alt="dark" width={20} height={20} />
-            <p>Light Mode</p></div>}
-      </button>
+        </button>
 
+      </div>
     </div>
-  );
+  )
+    ;
 };
 export default Navbar;
