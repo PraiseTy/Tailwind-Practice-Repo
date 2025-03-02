0b04258096666ef5ef84b5fa8418eec0970eade8
Praise <praisetoyo@gmail.com>
2025-03-02 23:13:27 +0100

    feat: finish country page and add border countries

--- Explore-Countries/src/app/country/[country]/page.tsx
+++ Explore-Countries/src/app/country/[country]/page.tsx
@@ -21,7 +21,7 @@ const CountryPage = ({ params }: CountryDetailsProps) => {
   return (
     <div className="mt-10 container mx-auto ">
       <div onClick={() => router.back()}
-           className="flex space-x-2 w-fit bg-[var(--color-navColor)] rounded-sm px-6 py-1 cursor-pointer mb-10 shadow-2xl max-sm:ml-9 ">
+           className="flex space-x-2 w-fit bg-[var(--color-navColor)]  rounded-sm px-6 py-1 cursor-pointer mb-10 shadow-2xl max-sm:ml-9 ">
         <Image src="/move-left.svg" alt="back-arrow" width={20} height={30} className={isDarkMode ? 'invert' : ''} />
         <p>Back</p>
       </div>
@@ -40,38 +40,41 @@ const CountryPage = ({ params }: CountryDetailsProps) => {
             :
             <p>No country flag available</p>}
         </div>
-        <div className="flex-1">
-          <div className="flex flex-row max-sm:flex-col ">
-            <div>
-              <h1>{country.name}</h1>
+        <div className="flex-1 max-sm:pl-10">
+          <div className="flex flex-row max-sm:flex-col space-x-16">
+            <div className='mt-10 space-y-3'>
+              <h1 className='text-2xl font-bold'>{country.name}</h1>
               <p><span className="font-semibold">Native Name</span>: {country.nativeName}</p>
               <p><span className="font-semibold">Population</span>: {country.population}</p>
               <p><span className="font-semibold">Region</span>: {country.region}</p>
               <p><span className="font-semibold">Sub Region</span>: {country.subregion}</p>
               <p><span className="font-semibold">Capital</span>: {country.capital}</p>
             </div>
-            <div>
+            <div className='mt-20 max-sm:mt-10 space-y-3'>
               <p><span className="font-semibold">Top Level Domain</span>: {country.topLevelDomain}</p>
               <p><span className="font-semibold">Currencies</span>: {country.currencies?.map(currency => currency.name)}
               </p>
-              <p><span
+              <p>
+                <span
                 className="font-semibold">Languages</span>: {country.languages?.map(language => `${language.name}`).join(' ')}
               </p>
             </div>
           </div>
-          <div>
-            <p><span className="font-semibold">Border Countries</span>:
+          <div className="mt-10 flex max-sm:flex-col flex-wrap gap-2 items-center max-sm:items-start ">
+            <p className="font-semibold ">Border Countries:</p>
+            <div className="flex flex-wrap gap-2 ">
               {country.borders && country.borders.length > 0 ? (
                 country.borders.map(borderCode => {
                   const borderCountry = countryData.find(c => c.alpha3Code === borderCode);
-                  return borderCountry ? `${borderCountry.name}` : `${borderCode}`;
-                }).join(' ')
-              ) : ' '}
-            </p>
+                  return borderCountry ? <button onClick={()=> router.push(`/country/${borderCountry.alpha3Code}`)} className=" bg-[var(--color-navColor)] px-3 py-1 rounded-sm shadow-2xl">{`${borderCountry.name}`}</button> :
+                    <div>{`${borderCode}`}</div>;
+                })
+              ) : 'No border Countries'}
+            </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
-export default CountryPage;
+      );
+      };
+      export default CountryPage;
