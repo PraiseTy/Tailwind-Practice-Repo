2366503b32cf142d130102e384cd1fb9b76f949f
Praise <praisetoyo@gmail.com>
2025-03-04 22:22:19 +0100

    feat: set up new project

---
+++ Conference-Ticket-Generator/src/components/Input/InputPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const InputPage = () => {
+    return (
+    <div>
+        <div className='bg-pink-500'>Input Page Yeah</div>
+    </div>
+    )
+}
+export default InputPage;
