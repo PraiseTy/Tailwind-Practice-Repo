8bca1c78b330d5e21e5e3acdee2372f3baf34628
Praise <praisetoyo@gmail.com>
2025-05-18 23:19:51 +0100

    feat: add navbar section

--- news-homepage/src/components/Navbar/Navbar.tsx
+++ news-homepage/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,27 @@
+import Image from 'next/image';
 const Navbar = () => {
   return (
-    <div>
-      <p>Navbar</p>
+    <div className="container mx-auto mt-10 md:px-24 px-4 flex justify-between items-center">
+      <Image src="/logo.svg" alt="logo" height={50} width={50} />
+      <Image src="/icon-menu.svg" alt="mobile-menu" height={50} width={50} className="md:hidden block" />
+
+      <div className="space-x-10 md:block hidden">
+        <a href="#" className="hover:text-soft-red">
+          Home
+        </a>
+        <a href="#" className="hover:text-soft-red">
+          New
+        </a>
+        <a href="#" className="hover:text-soft-red">
+          Popular
+        </a>
+        <a href="#" className="hover:text-soft-red">
+          Trending
+        </a>
+        <a href="#" className="hover:text-soft-red">
+          Categories
+        </a>
+      </div>
     </div>
   );
 };
