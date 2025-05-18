ce9ca1593e708d5a80ddcdc7b431d5783b84a70c
PraiseTy <111097676+PraiseTy@users.noreply.github.com>
2025-05-18 23:37:59 +0100

    feat: add navbar section
    * feat: add navbar section
    
    * chore: add idea files to gitignore
    
    * fix: make minor changes

--- news-homepage/src/app/page.tsx
+++ news-homepage/src/app/page.tsx
@@ -1,7 +1,9 @@
-import {Navbar} from "@/components";
+import { Navbar } from '@/components';
 
 export default function Home() {
   return (
-    <div><Navbar/></div>
+    <div>
+      <Navbar />
+    </div>
   );
 }
--- news-homepage/src/components/Navbar/Navbar.tsx
+++ news-homepage/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,40 @@
+import Image from 'next/image';
+import Link from 'next/link';
 const Navbar = () => {
   return (
-    <div>
-      <p>Navbar</p>
+    <div className="container mx-auto mt-10 md:px-24 px-4 flex justify-between items-center">
+      <Image src="/logo.svg" alt="logo" height={50} width={50} />
+      <Image src="/icon-menu.svg" alt="mobile-menu" height={50} width={50} className="md:hidden block" />
+
+      <nav className=" md:block hidden">
+        <ul className="flex flex-row space-x-10">
+          <li>
+            <Link href="#" className="hover:text-soft-red">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="#" className="hover:text-soft-red">
+              New
+            </Link>
+          </li>
+          <li>
+            <Link href="#" className="hover:text-soft-red">
+              Popular
+            </Link>
+          </li>
+          <li>
+            <Link href="#" className="hover:text-soft-red">
+              Trending
+            </Link>
+          </li>
+          <li>
+            <Link href="#" className="hover:text-soft-red">
+              Categories
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
