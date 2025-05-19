52566ec505c54a79e329f89d4739a185a2f1993a
PraiseTy <111097676+PraiseTy@users.noreply.github.com>
2025-05-19 23:26:34 +0100

    feat: add remaining sections (#2)
    * feat: add remaining sections
    
    * feat: make minor changes

---
+++ news-homepage/src/components/Articles/ArticleCard.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+
+interface ArticleProps {
+  img: string;
+  number: string;
+  title: string;
+  body: string;
+}
+
+const ArticleCard = ({ img, number, title, body }: ArticleProps) => {
+  return (
+    <div className="flex flex-row items-stretch gap-3 ">
+      <Image src={img} alt="article image" height={200} width={200} className="w-auto" />
+      <div className="space-y-2">
+        <p className="text-soft-red text-3xl font-bold">{number}</p>
+        <h3 className="text-dark-blue text-lg font-bold max-w-full hover:text-soft-red">{title}</h3>
+        <p>{body}</p>
+      </div>
+    </div>
+  );
+};
+export default ArticleCard;
---
+++ news-homepage/src/components/Articles/ArticleData.ts
@@ -0,0 +1,20 @@
+export const ArticleData = [
+  {
+    img: '/image-retro-pcs.jpg',
+    number: '01',
+    title: 'Reviving Retro PCs',
+    body: 'What happens when old PCs are given modern upgrades '
+  },
+  {
+    img: '/image-top-laptops.jpg',
+    number: '02',
+    title: 'Top 10 Laptops of 2022',
+    body: 'Our best picks for various needs and budgets. '
+  },
+  {
+    img: '/image-gaming-growth.jpg',
+    number: '03',
+    title: 'The Growth of Opportunities',
+    body: 'How the pandemic has sparked fresh opportunities'
+  }
+];
